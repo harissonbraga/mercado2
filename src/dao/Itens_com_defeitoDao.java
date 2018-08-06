59272a6c98af2b8a4f028ffd3be60ca1b3b65c97
631201162 <631201162@LAB30820.senacinformatica.br>
2018-08-06 10:23:21 -0300

    itens com defeito

--- src/dao/Itens_com_defeitoDao.java
+++ src/dao/Itens_com_defeitoDao.java
@@ -5,10 +5,40 @@
  */
 package dao;
 
+import obj.ObjEstoque;
+
 /**
  *
  * @author 181710062
  */
 public class Itens_com_defeitoDao {
+    public static void inserir( ObjEstoque est){
+        String sql = "INSERT INTO estoque "
+                + " ( nome, cor, codigo, data de vencimento ) "
+                + " VALUES ( "
+                + "  '" + est.getNome()               + "' , "
+                + "  '" + est.getCor()+ "' , "
+                + "  '" + est.getCodigo()+ "' , "
+                + "  '" + est.getData_de_vencimento()+ "    "
+                
+                + " );" ;
+        Conexao.executar( sql );
+    }
     
+     public static void editar( ObjEstoque est ){
+        String sql = "UPDATE estoque SET "
+                + " nome =      '" + est.getNome()               + "' , "
+                + " cor =  '" + est.getCor()+ "' , "
+                + " codigo =  '" + est.getCodigo()+ "' , "
+                + " data de vencimento =      '" + est.getData_de_vencimento()+ "    "
+                
+                + " WHERE codigo = " + est.getCodigo() ;
+        Conexao.executar( sql );
+    }
+     
+      public static void excluir( ObjEstoque est){
+        String sql = "DELETE FROM estoque "
+                + " WHERE codigo = " + est.getCodigo() ;
+        Conexao.executar( sql );
+    }
 }
