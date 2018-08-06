0134b82b939ffa02b8d05265a0d5f705e002b2a4
631201162 <631201162@LAB30820.senacinformatica.br>
2018-08-06 10:59:13 -0300

    itens com defeito

--- src/dao/Itens_com_defeitoDao.java
+++ src/dao/Itens_com_defeitoDao.java
@@ -1,37 +1,34 @@
-
 package dao;
 
 import obj.ObjEstoque;
 
-
 public class Itens_com_defeitoDao {
-    public static void inserir( ObjEstoque est){
+
+    public static void inserir(ObjEstoque est) {
         String sql = "INSERT INTO estoque "
                 + " ( nome, cor, codigo, data de vencimento ) "
                 + " VALUES ( "
-                + "  '" + est.getNome()               + "' , "
-                + "  '" + est.getCor()+ "' , "
-                + "  '" + est.getCodigo()+ "' , "
-                + "  '" + est.getData_de_vencimento()+ "    "
-                
-                + " );" ;
-        Conexao.executar( sql );
+                + "  '" + est.getNome() + "' , "
+                + "  '" + est.getCor() + "' , "
+                + "  '" + est.getCodigo() + "' , "
+                + "  '" + est.getData_de_vencimento() + "    "
+                + " );";
+        Conexao.executar(sql);
     }
-    
-     public static void editar( ObjEstoque est ){
+
+    public static void editar(ObjEstoque est) {
         String sql = "UPDATE estoque SET "
-                + " nome =      '" + est.getNome()               + "' , "
-                + " cor =  '" + est.getCor()+ "' , "
-                + " codigo =  '" + est.getCodigo()+ "' , "
-                + " data de vencimento =      '" + est.getData_de_vencimento()+ "    "
-                
-                + " WHERE codigo = " + est.getCodigo() ;
-        Conexao.executar( sql );
+                + " nome =      '" + est.getNome() + "' , "
+                + " cor =  '" + est.getCor() + "' , "
+                + " codigo =  '" + est.getCodigo() + "' , "
+                + " data de vencimento =      '" + est.getData_de_vencimento() + "    "
+                + " WHERE codigo = " + est.getCodigo();
+        Conexao.executar(sql);
     }
-     
-      public static void excluir( ObjEstoque est){
+
+    public static void excluir(ObjEstoque est) {
         String sql = "DELETE FROM estoque "
-                + " WHERE codigo = " + est.getCodigo() ;
-        Conexao.executar( sql );
+                + " WHERE codigo = " + est.getCodigo();
+        Conexao.executar(sql);
     }
 }
