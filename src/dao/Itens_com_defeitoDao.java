1430a54dc369661bfe8005e7bb405b2938c775a8
631201162 <631201162@LAB30820.senacinformatica.br>
2018-08-06 10:31:30 -0300

    itens com defeito

--- src/dao/Itens_com_defeitoDao.java
+++ src/dao/Itens_com_defeitoDao.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dao;
 
 import obj.ObjEstoque;
 
-/**
- *
- * @author 181710062
- */
+
 public class Itens_com_defeitoDao {
     public static void inserir( ObjEstoque est){
         String sql = "INSERT INTO estoque "
